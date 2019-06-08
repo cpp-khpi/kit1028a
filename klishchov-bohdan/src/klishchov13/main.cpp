be70668f143f33afa55492519121f198e13f1a71
klishchov-bohdan <47816921+klishchov-bohdan@users.noreply.github.com>
2019-06-08 23:53:36 +0300

    #13 klishchov13: переделан исходный код (´｡• ~ •｡`)

--- klishchov-bohdan/src/klishchov13/main.cpp
+++ klishchov-bohdan/src/klishchov13/main.cpp
@@ -4,18 +4,21 @@
 #include <map>
 #include <list>
 #include <iterator>
-#include <algorithm>
 #include "Country.h"
 
 using namespace std;
 
-int main() {
+struct Print {
+	Print(){}
+	void operator()(Country printCountry) { cout << printCountry << " "; cout << endl; }
+
+};
+
+void Vector() {
 	vector<Country> myVector;
 	int choose, id;
 	Country country;
-	std::set<Country> mySet;
-	std::list<Country> myList;
-
+	string name;
 	cout << "Vector" << endl;
 	do {
 		cout << "0 - Exit" << endl;
@@ -23,15 +26,14 @@ int main() {
 		cout << "2 - Find element by index" << endl;
 		cout << "3 - Add element" << endl;
 		cout << "4 - Delete element" << endl;
+		cout << "5 - Number of elements" << endl;
 		cout << "Choose: ";
 		cin >> choose;
 
 		switch (choose)
 		{
 		case 1:
-			for (int i = 0; i < myVector.size(); i++) {
-				cout << myVector[i] << endl;
-			}
+			for_each(myVector.begin(), myVector.end(), Print());
 			break;
 		case 2:
 			cout << "input index: ";
@@ -39,18 +41,30 @@ int main() {
 			cout << myVector.at(id);
 			break;
 		case 3: {
-			country.setData("Japan");
+			cout << "Please, enter the name of country: ";
+			cin >> name;
+			cout << endl;
+			country.setData(name);
 			myVector.push_back(country);
 			break;
 		}
 		case 4:
 			myVector.clear();
 			break;
+		case 5:
+			cout << "Number of elements is: " << myVector.size() << endl;
+			break;
 		default:
 			break;
 		}
 	} while (choose != 0);
+}
 
+void Set() {
+	int choose, id;
+	set<Country> mySet;
+	Country country;
+	string name;
 	cout << "Set" << endl;
 
 	do {
@@ -58,29 +72,42 @@ int main() {
 		cout << "1 - Print" << endl;
 		cout << "2 - Add element" << endl;
 		cout << "3 - Delete element" << endl;
+		cout << "4 - Number of elements" << endl;
 		cout << "Choose: ";
 		cin >> choose;
 
 		switch (choose)
 		{
 		case 1:
-			for (auto &it : mySet) {//öèêë for_each
-				cout << it << "\t";
+			for (auto &it : mySet) {
+				cout << it << endl;
 			}
 			break;
 		case 2: {
-			country.setData("Japan");
+			cout << "Please, enter the name of country: ";
+			cin >> name;
+			cout << endl;
+			country.setData(name);
 			mySet.insert(country);
 			break;
 		}
 		case 3:
 			mySet.erase(country);
 			break;
+		case 4:
+			cout << "Number of elements is: " << mySet.size() << endl;
+			break;
 		default:
 			break;
 		}
 	} while (choose != 0);
+}
 
+void List() {
+	int choose, id;
+	list<Country> myList;
+	Country country;
+	string name;
 	cout << "List" << endl;
 	do {
 		cout << "0 - Exit" << endl;
@@ -88,8 +115,11 @@ int main() {
 		cout << "2 - Find element by index" << endl;
 		cout << "3 - Add element" << endl;
 		cout << "4 - Delete element" << endl;
+		cout << "5 - Number of elements" << endl;
 		cout << "Choose: ";
 		cin >> choose;
+
+		auto it = myList.begin();
 
 		switch (choose)
 		{
@@ -102,17 +132,16 @@ int main() {
 			cout << "input index: ";
 			cin >> id;
 
-			auto it = myList.begin();
 			advance(it, id);
 
 			cout << id;
 			break;
 		case 3: {
-			auto it = myList.begin();
-			cout << "Input index: ";
-			cin >> id;
-			advance(it, id);
-
+			it = myList.begin();
+			cout << "Please, enter the name of country: ";
+			cin >> name;
+			cout << endl;
+			country.setData(name);
 			myList.insert(it, country);
 
 			break;
@@ -124,6 +153,98 @@ int main() {
 
 			advance(it, id);
 			myList.erase(it);
+			break;
+		case 5:
+			cout << "Number of elements is: " << myList.size() << endl;
+			break;
+		default:
+			break;
+		}
+	} while (choose != 0);
+}
+
+void Map() {
+	int choose, id;
+	map<int, Country> myMap;
+	Country country;
+	string name;
+	cout << "Map" << endl;
+	do {
+		cout << "0 - Exit" << endl;
+		cout << "1 - Print" << endl;
+		cout << "2 - Find element by index" << endl;
+		cout << "3 - Add element" << endl;
+		cout << "4 - Delete element" << endl;
+		cout << "5 - Number of elements" << endl;
+		cout << "Choose: ";
+		cin >> choose;
+
+		auto it = myMap.begin();
+		int k;
+
+		switch (choose)
+		{
+		case 1:
+			for (const auto& el : myMap) {
+				std::cout << el.second << endl;
+			}
+			break;
+		case 2:
+			cout << "Input index: ";
+			cin >> id;
+			cout << myMap.at(id);
+			break;
+		case 3: {
+			cout << "Please, enter key: ";
+			cin >> k;
+			cout << endl;
+			cout << "Please, enter the name of country: ";
+			cin >> name;
+			cout << endl;
+			country.setData(name);
+			myMap.emplace(k, country);
+			break;
+		}
+		case 4:
+			cout << "input delete point: ";
+			cin >> id;
+			myMap.erase(id);
+			break;
+		case 5:
+			cout << "Number of elements is: " << myMap.size() << endl;
+			break;
+		default:
+			break;
+		}
+	} while (choose != 0);
+}
+
+int main() {
+	int choose;
+
+	do {
+		cout << "0 - Exit" << endl;
+		cout << "1 - Work with VECTOR" << endl;
+		cout << "2 - Work with SET" << endl;
+		cout << "3 - Work with LIST" << endl;
+		cout << "4 - Work with MAP" << endl;
+		cout << "Choose: ";
+		cin >> choose;
+
+		switch (choose)
+		{
+		case 1:
+			Vector();
+			break;
+		case 2:
+			Set();
+			break;
+		case 3: {
+			List();
+			break;
+		}
+		case 4:
+			Map();
 			break;
 		default:
 			break;
