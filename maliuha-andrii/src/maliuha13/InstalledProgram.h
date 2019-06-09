6db2ca582b79d950768f8c85a8ddfc9e0f006ec3
maliuha-andrii <47816379+maliuha-andrii@users.noreply.github.com>
2019-06-09 21:34:12 +0300

    #1 maliuha13: программа готова к проверке

---
+++ maliuha-andrii/src/maliuha13/InstalledProgram.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Main.h"
+
+class InstalledProgram{
+private:
+	string name;
+	string publisher;
+	float hDisk;
+	Version version;
+public:
+	InstalledProgram();
+	string print();
+	void setInfoObj(string &info);
+	void setObj(string &info);
+	void show();
+	
+	void setName(string name);
+	void setPublisher(string publisher);
+	void setHDisk(float hDisk);
+	void setVersion(Version version);
+
+	string getName();
+	string getPublisher();
+	float getHDisk();
+	Version getVersion();
+
+	InstalledProgram& operator= (const InstalledProgram &obj);
+	friend bool operator< (const InstalledProgram &obj1, const InstalledProgram &obj2);
+	friend istream& operator>> (istream &in, InstalledProgram &obj);
+	friend ostream& operator<< (ostream &in, const InstalledProgram &obj);
+};
